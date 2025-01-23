a031c09dae70e6737e966f67256ad8d747d8d855
deV-ing08 <vintd2004@gmail.com>
2025-01-23 13:15:53 +0700

    router http module

--- ServerNodeBasic/httpServer.js
+++ ServerNodeBasic/httpServer.js
@@ -1,14 +1,32 @@
 // http core modules
 const http = require('http')
 
+// Routes with http module
+const routes = {
+    '/': function(request, response) {
+        response.writeHead(200, { 'Content-Type': 'text/plain' })
+        response.write('This is HOME page')
+        response.end()
+    },
+    '/foo': function(request, response) {
+        response.writeHead(200, { 'Content-Type': 'text/plain' })
+        response.write('This is Foo page')
+        response.end()
+    }
+}
+
 http.createServer((request, response) => {
-    response.writeHead(200, {
-        'Content-Type': 'text/plain'
-    });
 
-    response.write('Hello world');
+    for(let route in routes) {
+        if(request.url === route) {
+            return routes[route](request, response)
+        }
+    };
 
+    response.writeHead(404, { 'Content-Type': 'text/plain' })
+    response.write('Not found')
     response.end();
+
 }).listen(1122);
 
 
