3810de70cc02581f6cea4ebfa0335cdbaf65e181
deV-ing08 <vintd2004@gmail.com>
2025-01-25 09:05:05 +0700

    build in modules

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const name = require('./module/module1')
 const { getInfor, sayBye } = require('./module/moduls2')
 const { items, singlePerson } = require('./module/module3')
+require('./module/module4')
 
 console.log(items)
 
