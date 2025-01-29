0bf558a8cebd4f6f2ce984be38d2c064efef2d73
deV-ing08 <vintd2004@gmail.com>
2025-01-29 13:17:27 +0700

    loop with async/await

--- async_nodejs/index.js
+++ async_nodejs/index.js
@@ -3,7 +3,7 @@ function shortTime() {
     return new Promise((resolve) => {
         setTimeout(() => {
             resolve('resultShortTime')
-        }, 200);
+        }, 1000);
     })
 };
 
@@ -11,7 +11,7 @@ function mediumTime() {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
             resolve('resultMediumTime')
-        }, 700)
+        }, 200)
     })
 };
 
@@ -20,7 +20,7 @@ function longTime() {
     return new Promise((resolve) => {
         setTimeout(() => {
             resolve('resultLongTime')
-        }, 1000)
+        }, 3000)
     })
 }
 
@@ -57,10 +57,7 @@ async function Parallel() {
     try {
         
         // recived result => Successfully
-        const [ result1, result2, result3 ] = await Promise.all([shortTime(), mediumTime(), longTime()])
-        if(result1) {
-            console.log('hehehhe')
-        }
+        const [ result1, result2, result3 ] = await Promise.all([shortTime(), mediumTime(), longTime()]) // 
         console.log('result1:', result1)
         console.log("result2:", result2)
         console.log('result3:', result3)
@@ -74,5 +71,71 @@ async function Parallel() {
 Parallel()
 
 // Series Excution
-function Series() {
+async function Series() {
+   try {
+    const result1 = await shortTime()
+    const result2 = await mediumTime()
+    const result3 = await longTime()
+    console.log(result1)
+    console.log(result2)
+    console.log(result3)
+   } catch(error) {
+    // if error
+   }
 }
+
+Series()
+
+
+// Waterfall Execution
+function getUser() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve({ name: 'Mqa' })
+        }, 1000);
+    })
+}
+
+function getFriendsFromUser(user) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            if(user.name === 'Mqa') {
+                resolve([{ name: 'Vinh' }, { name: 'Cong' }])
+            } else {
+                resolve([])
+            }
+        }, 1000)
+    })
+}
+
+
+async function waterfallExcution() {
+    const user = await getUser();
+    const listFriends = await getFriendsFromUser(user);
+    console.log(listFriends)
+}
+
+
+// handle loop with asycn/await
+function recursiveAction(n) {
+    return new Promise((resolve) => {
+        resolve('result' + n)
+    })
+}
+
+// loop and push result in []
+async function runLoop(n) {
+    try {
+        let results = [];
+        for(let i = 1; i <= n; i++) {
+            let result = await recursiveAction(i);
+            results.push(result) 
+        }
+        console.log(results)
+    
+    } catch(err) {
+        console.log('Error:', err)        
+    }
+}
+
+runLoop(5)
